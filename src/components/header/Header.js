3882fb0b44c39b185d258348950a3cb621354725
ankit.chaudhary3 <ankit.chaudhary@thoughtworks.com>
2022-07-06 12:33:04 +0530

    Add. microphone icon in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './styles/HeaderStyles.css';
 import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 import SearchIcon from '@mui/icons-material/Search';
+import MicIcon from '@mui/icons-material/Mic';
 
 function Header() {
   return (
     <div className='header'>
         <div className='headerItems'>
-        <DensityMediumIcon className="menuIcon" />
-        <img src='/yt_logo.png' height='50px'/>
+            <DensityMediumIcon className="menuIcon" />
+            <img src='/yt_logo.png' height='50px'/>
         </div>
         <div className="headerItems">
             <input type="text" placeholder="Search" className="searchBox" /> 
             <SearchIcon className='searchIcon'/>
+            <MicIcon className='micIcon' />
         </div>
     </div>
   )
