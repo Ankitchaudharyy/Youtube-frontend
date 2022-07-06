7c1e47b59c11004fed98038a93ef16ef74749e3c
ankit.chaudhary3 <ankit.chaudhary@thoughtworks.com>
2022-07-06 13:05:33 +0530

    Add. app icons in the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,10 @@ import './styles/HeaderStyles.css';
 import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
+import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 function Header() {
   return (
@@ -11,10 +15,16 @@ function Header() {
             <DensityMediumIcon className="menuIcon" />
             <img src='/yt_logo.png' height='50px'/>
         </div>
-        <div className="headerItems">
+        <div className="searchField">
             <input type="text" placeholder="Search" className="searchBox" /> 
             <SearchIcon className='searchIcon'/>
             <MicIcon className='micIcon' />
+        </div>
+        <div className='iconMenu'>
+            <VideoCallOutlinedIcon fontSize="large" sx={{ stroke: "#ffffff", strokeWidth: 1 }} className="icon" />
+            <AppsOutlinedIcon fontSize="large" sx={{ stroke: "#ffffff", strokeWidth: 1 }} className="icon" />
+            <NotificationsOutlinedIcon fontSize="large" sx={{ stroke: "#ffffff", strokeWidth: 0.5 }} className="icon" />
+            <AccountCircleOutlinedIcon fontSize="large" sx={{ stroke: "#ffffff", strokeWidth: 0.5 }} className="icon" />
         </div>
     </div>
   )
