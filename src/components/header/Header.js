c0b24f02851f1e1abd4e3e3f16fd99454f908f1e
ankit.chaudhary3 <ankit.chaudhary@thoughtworks.com>
2022-07-06 11:16:57 +0530

    Add. menu icon in header

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/HeaderStyles.css';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+
+function Header() {
+  return (
+    <div>
+        <DensityMediumIcon className="menuIcon" />
+    </div>
+  )
+}
+
+export default Header
