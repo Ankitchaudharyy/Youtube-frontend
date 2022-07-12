9e658fddd34174fbf37dbbbbd4a9ffd42083f33a
ankit.chaudhary3 <ankit.chaudhary@thoughtworks.com>
2022-07-12 17:06:24 +0530

    [Ankit|Praveen] Add. Youtube API integration to search videos.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShowMenuContext } from '../../context/SideMenuContext';
 import VideoCard from '../videoCard/VideoCard';
 import styles from './styles/HomeStyles.module.css';
+import { fetchVideos } from '../../services/SearchVideoService';
 
 function Home() {
 
-    const { showMenu } = useContext(ShowMenuContext)
+    const [videos, setVideos] = useState([]);
 
-    console.log(showMenu)
+    useEffect(() => {
+        fetchVideos("react")
+            .then(response => {
+                setVideos(response.data.items)
+            }).catch(err => {
+                console.log(err);
+            })
+    }, [])
+
+    const { showMenu } = useContext(ShowMenuContext)
 
     return (
         <div className={`${styles.home} `.concat(showMenu ? `${styles.collapsedHomePage}` : `${styles.expandedHomePage}`)}>
---
+++ src/services/SearchVideoService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+const youtubeSearch = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part: 'snippet',
+        maxResults: 24,
+        key: KEY
+    }
+})
+
+const fetchVideos = async (searchText) => {
+    const response = await youtubeSearch.get('/search', {
+        params: {
+            q: searchText
+        }
+    })
+    return response;
+}
+
+export { fetchVideos }
+
