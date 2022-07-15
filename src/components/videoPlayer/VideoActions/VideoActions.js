bc2ec666ccf4c7ecbe39d7e6f83a646c76bfb2e1
Praveen Raj <praveen.raj@thoughtworks.com>
2022-07-15 16:35:28 +0530

    [Ankit | Praveen] Add. tooltip to action icons

--- src/components/videoPlayer/VideoActions/VideoActions.js
+++ src/components/videoPlayer/VideoActions/VideoActions.js
@@ -2,20 +2,18 @@ import React from 'react';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import EmojiFlagsIcon from '@mui/icons-material/EmojiFlags';
 import { actionIcons } from './ActionIcons';
-import Tooltip from '@mui/material/Tooltip';
 import styles from './styles/VideoActionsStyles.module.css'
 
 function VideoActions() {
     return (
         <div className={styles.videoActions}>
             {
-                actionIcons.map((actionIcon,index)=>{
+                actionIcons.map((actionIcon, index) => {
                     return (
                         <div key={index} className={styles.videoActionItem}>
-                            {/* <Tooltip title={actionIcon.toolTipText} placement="bottom"> */}
-                                {actionIcon.outlinedIcon} 
-                                <span className={styles.iconName}>{actionIcon.name}</span>
-                            {/* </Tooltip> */}
+                            {actionIcon.outlinedIcon}
+                            <span className={styles.iconName}> {actionIcon.name} </span>
+                            <span className={styles.tooltiptext}> {actionIcon.toolTipText} </span>
                         </div>
                     )
                 })
