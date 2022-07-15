2ac4e46253812da90a8342903e8348f1b5a8688a
Praveen Raj <praveen.raj@thoughtworks.com>
2022-07-15 11:17:03 +0530

    [Ankit | Praveen] Add. date formatter in MMDDYYYY

--- src/helpers/DateFormatHelper.js
+++ src/helpers/DateFormatHelper.js
@@ -3,7 +3,11 @@ import moment from 'moment';
 function dateDifference(startingDate) {
     const formattedDate = moment(startingDate).format();
     return `${moment(formattedDate).toNow(true)} ago`;
-    
+
 }
 
-export { dateDifference }
+function formatDateInMMDDYY(date) {
+    return moment(date).format('MMMM DD, YYYY');
+}
+
+export { dateDifference, formatDateInMMDDYY }
--- src/helpers/VideoDetailsHelper.js
+++ src/helpers/VideoDetailsHelper.js
@@ -1,11 +1,12 @@
 import { randomViews } from './ViewsGeneratorHelper';
-import { dateDifference } from './DateDifferenceHelper';
+import { dateDifference , formatDateInMMDDYY } from './DateFormatHelper';
 
 const getVideoDetails = (videosData) => {
     return videosData.filter(video => {
         return video.id.videoId !== undefined
     })
         .map(video => {
+            console.log(formatDateInMMDDYY(video.snippet.publishedAt))
             return {
                 title: video.snippet.title,
                 channelTitle: video.snippet.channelTitle,
@@ -13,7 +14,7 @@ const getVideoDetails = (videosData) => {
                 id: video.id.videoId,
                 views: randomViews(),
                 publishedWhen: dateDifference(video.snippet.publishedAt),
-                publishedDate: video.snippet.publishedAt
+                publishedDate: formatDateInMMDDYY(video.snippet.publishedAt)
             }
         })
         .slice(0, 24)
