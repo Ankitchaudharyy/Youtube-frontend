f3fc18877c8dcbbf887f91c9f85a0d3a61b1654e
Praveen Raj <praveen.raj@thoughtworks.com>
2022-07-15 10:34:58 +0530

    [Ankit | Praveen] Fix. import statements

---
+++ src/components/videoSuggestions/VideoSuggestions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './styles/VideoSuggestionsStyles.module.css';
+
+function VideoSuggestions() {
+    return (
+        <div className={styles.suggestions}>
+        </div>
+    )
+}
+
+export default VideoSuggestions
