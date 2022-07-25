251cf89cad6038eaf0f5db4a6feb468244910538
ankit.chaudhary3 <ankit.chaudhary@thoughtworks.com>
2022-07-25 11:20:19 +0530

    [Ankit|Praveen] Add. functionality for fetching videos when searched

--- src/components/searchResults/SearchResults.js
+++ src/components/searchResults/SearchResults.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import { useLocation } from 'react-router'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router';
+import { fetchVideos } from './../../services/SearchVideoService';
+import { getVideoDetails } from './../../helpers/VideoDetailsHelper'
 
 function SearchResults() {
-  const searchText = useLocation().state.searchText;
-  
-  return (
-    <div>SearchResults</div>
-  )
+	const searchText = useLocation().state.searchText;
+
+	const [searchResults, setSearchResults] = useState([]);
+
+	useEffect(() => {
+		fetchVideos(searchText)
+			.then(response => {
+				setSearchResults(getVideoDetails(response.data.items))
+			}).catch(err => {
+				console.log(err);
+			})
+	}, [searchText]);
+
+	return (
+		<div>
+			{
+				searchResults.map((searchResult, index) => {
+					return <div key={index}>
+						{searchResult.title}
+					</div>
+				})
+			}
+		</div>
+	)
 }
 
 export default SearchResults
