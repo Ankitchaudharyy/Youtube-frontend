8c4ab59e54a832fa48787f79396c9c40f0d2f098
ankit.chaudhary3 <ankit.chaudhary@thoughtworks.com>
2022-07-26 11:33:02 +0530

    [Ankit|Praveen] Add. navigation from search results to video player.

--- src/components/searchResults/videoResult/VideoResult.js
+++ src/components/searchResults/videoResult/VideoResult.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Thumbnail from '../../thumbnail/Thumbnail';
 import VideoDetails from '../../videoDetails/VideoDetails';
 import styles from './styles/VideoResultStyles.module.css';
+import {useNavigate} from 'react-router';
 
 function VideoResult({ videoDetails }) {
+    let navigate = useNavigate();
+
+    const navigateToVideoPlayer = () => navigate(`/video/${videoDetails.id}`, { state: { video: videoDetails } })
+
     return (
-        <div className={styles.videoResult}>
+        <div className={styles.videoResult} onClick={navigateToVideoPlayer}>
             <div className={styles.thumbnail}>
                 <Thumbnail thumbnailUrl={videoDetails.thumbnailUrl} height="90%" />
             </div>
