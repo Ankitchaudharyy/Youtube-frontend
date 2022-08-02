7042153b3417fab084ace71c22920228a00523f5
ankit.chaudhary3 <ankit.chaudhary@thoughtworks.com>
2022-08-02 14:54:16 +0530

    [Ankit | Praveen] Add. link for navigation to home page in side menu

--- src/components/sideMenu/collapsedSideMenu/CollapsedSideMenuOption.js
+++ src/components/sideMenu/collapsedSideMenu/CollapsedSideMenuOption.js
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react';
 import { SelectedOptionContext } from '../../../context/SideMenuContext';
-import './styles/CollapsedSideMenuOptionStyles.css'
+import './styles/CollapsedSideMenuOptionStyles.css';
+import {useNavigate} from 'react-router';
 
 function CollapsedSideMenuOption({ text, Icon, OutlinedIcon }) {
-
+    const navigate = useNavigate();
     const {selected, setSelected} = useContext(SelectedOptionContext);
 
+    const handleClick = () => {
+        setSelected(text);
+        if(text === "Home")
+          navigate('/Youtube-frontend');
+      }
+
     return (
-        <div className="collapsedMenuOption" onClick={() => setSelected(text)}>
+        <div className="collapsedMenuOption" onClick={handleClick}>
             <div>
                 {selected === text ? Icon : OutlinedIcon}
             </div>
--- src/components/sideMenu/expandedSideMenu/ExpandedSideMenuOption.js
+++ src/components/sideMenu/expandedSideMenu/ExpandedSideMenuOption.js
@@ -2,15 +2,24 @@ import React, { useContext } from 'react'
 import './styles/ExpandedSideMenuOptionStyles.css';
 import Tooltip from '@mui/material/Tooltip';
 import { SelectedOptionContext } from '../../../context/SideMenuContext';
+import {useNavigate} from 'react-router';
 
 function ExpandedSideMenuOption({ text, Icon, OutlinedIcon }) {
+  const navigate = useNavigate();
+
   const {selected, setSelected} = useContext(SelectedOptionContext);
   
   const sel = selected === text ? "selectedOption" : "";
 
+  const handleClick = () => {
+    setSelected(text);
+    if(text === "Home")
+      navigate('/Youtube-frontend');
+  }
+
   return (
     <Tooltip title={text} placement="bottom-end">
-      <div className={`menuOption ${sel}`} onClick={() => setSelected(text)}>
+      <div className={`menuOption ${sel}`} onClick={handleClick}>
         <div className='sideIcon'>
           {selected === text ? Icon : OutlinedIcon}
         </div>
